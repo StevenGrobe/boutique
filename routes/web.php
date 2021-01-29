5636347d836c8ed0400fdc685c79cfb77f2f26ce
StevenGrobe <grobesteven@gmail.com>
2021-01-29 14:22:22 +0100

    step 3 - correction controller
    les uses en haut du fichier

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,12 +22,7 @@ Route::get('/hello', function () {
 });
 
 // Mise en place des URL de la boutique en créant des routes
-use App\Http\Controllers\HomeController;
 Route::get('/', [HomeController::class, 'index']);
-
-use App\Http\Controllers\ProductController;
 Route::get('/product', [ProductController::class, 'index']);
 Route::get('/product/{id}', [ProductController::class, 'id']);
-
-use App\Http\Controllers\CartController;
 Route::get('/cart', [CartController::class, 'index']);
