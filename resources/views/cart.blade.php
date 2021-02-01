eaa3e28d2121c29cd8d0a8f656f056db7786f59f
StevenGrobe <steven.grobe@le-campus-numerique.fr>
2021-02-01 16:49:18 +0100

    Panier + Produits HTML CSS

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,1 +1,97 @@
-<h1>Panier</h1>
+<!-- link wave design -->
+@extends("app")
+<!-- link for head -->
+@section("head")
+    @parent
+    <link rel="stylesheet" href="{{asset("/css/cart.css")}}">
+@endsection
+
+<!-- page name -->
+@section("page-title","Panier")
+
+<!-- Content -->
+@section("content")
+    <div class="cart">
+
+      <!-- recap -->
+      <div class="recap">
+        <div class="flex-zone">
+            <div class="item-image">
+              <img src={{asset("/images/cart.svg")}} width="48px" alt="">
+            </div>
+            <div class="item-text">
+              <h2>Récapitulatif des produits</h2>
+            </div>
+        </div>
+      </div>
+
+
+      <!-- produit -->
+      <div class="item">
+        <div class="flex-zone">
+            <div class="item-image">
+                <img src={{asset("/images/bento-1.svg")}} alt="">
+            </div>
+            <div class="item-text">
+              <div class="trash">
+                <i class="fa fa-trash"></i>
+              </div>
+                <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna.</p>
+                <p>9.80 €</p>
+                <div class="note">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <input class="quantity" id="id_form-0-quantity" min="1" max="10" name="form-0-quantity" value="1" type="number">
+          </div>
+        </div>      
+      </div>
+
+      <div class="item">
+        <div class="flex-zone">
+            <div class="item-image">
+                <img src="{{asset("/images/bento-1.svg")}}" alt="">
+            </div>
+            <div class="item-text">
+                <div class="trash">
+                <i class="fa fa-trash"></i>
+                </div>
+                <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna.</p>
+                <p>19.60€</p>
+                <div class="note">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <input class="quantity" id="id_form-0-quantity" min="1" max="10" name="form-0-quantity" value="2" type="number">
+          </div>
+        </div>      
+      </div>      
+    </div>
+
+
+    <!-- total -->
+    <div class="total">
+      <div class="flex-zone">
+          <div class="item-text">
+            <h2>Total : 29.40 €</h2>
+            <button class="btn-primary">Acheter</button>
+          </div>
+      </div>
+    </div>
+@endsection
+
+<!-- link for footer -->
+@section("footer")
+    @parent
+@endsection
+
--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -1,1 +1,99 @@
-<h1>Liste des produits</h1>
+@extends("app")
+
+@section("head")
+    @parent
+    <link rel="stylesheet" href="{{asset("/css/catalogue.css")}}">
+@endsection
+
+@section("page-title","Catalogue")
+
+@section("content")
+<div class="list-product">
+    <div class="item">
+        <div class="flex-zone">
+            <div class="item-image">
+                <img src="./images/bento-1.svg" alt="">
+            </div>
+            <div class="item-container">
+                <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat. Duis
+                    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <div class="note">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <button class="btn-primary">Découvrir</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="flex-zone">
+            <div class="item-image">
+                <img src="./images/bento-1.svg" alt="">
+            </div>
+            <div class="item-container">
+                <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat. Duis
+                    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <div class="note">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <button class="btn-primary">Découvrir</button>
+            </div>
+        </div>
+    </div>
+    <div class="item">
+        <div class="flex-zone">
+            <div class="item-image">
+                <img src="./images/bento-1.svg" alt="">
+            </div>
+            <div class="item-container">
+                <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat. Duis
+                    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <div class="note">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <button class="btn-primary">Découvrir</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+
+@section("footer")
+    @parent
+@endsection
+
