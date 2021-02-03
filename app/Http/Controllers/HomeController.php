118df9a52f4e36e2fede1216fa0de909071fb1d6
StevenGrobe <steven.grobe@le-campus-numerique.fr>
2021-02-03 20:50:20 +0100

    sql debut

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     //
     public function index()
     {
+        $users = DB::select('select * from products');
+
+        foreach ($users as $user) {
+            echo $user->name . '<br />';
+            echo $user->price . '<br />';
+            echo $user->picture . '<br />';
+            echo '<br />';
+        }
         return view('homepage');
     }
 }
